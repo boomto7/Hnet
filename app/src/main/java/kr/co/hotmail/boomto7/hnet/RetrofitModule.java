0e1228748fb9f63de63ede3ec988fcee81a17985
changgi, Hong <boomto7@hotmail.co.kr>
2021-08-23 12:49:53 +0900

    Add default classes

--- app/src/main/java/kr/co/hotmail/boomto7/hnet/RetrofitModule.java
+++ app/src/main/java/kr/co/hotmail/boomto7/hnet/RetrofitModule.java
@@ -13,7 +13,7 @@ import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class RetrofitModule<E> {
+public class RetrofitModule {
     private Map<String, Object> counter = new HashMap<String, Object>();
 
     @SuppressLint("StaticFieldLeak")
